d0d4865c1084b421a5c8aa6ff691d6ebc74ec555
Nick Stolarow <nickstolarow@u.boisestate.edu>
2020-09-28 23:32:21 -0600

    trying to make pictures work

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 	require_once "header.php";
 ?>
 		<div id="homeDiv">
-		<img id="homePics" src="1.jpg" />
-		<img id="homePics" src="2.jpg" />
-		<img id="homePics" src="3.jpg" />
-		<img id="homePics" src="4.jpg" />
-		<img id="homePics" src="5.jpg" />
-		<img id="homePics" src="6.jpg" />
-		<img id="homePics" src="7.jpg" />
-		<img id="homePics" src="8.jpg" />
-		<img id="homePics" src="9.jpg" />
-		<img id="homePics" src="10.jpg" />
+		<img id="homePics" src="./pictures/1.jpg" />
+		<img id="homePics" src="./pictures/2.jpg" />
+		<img id="homePics" src="./pictures/3.jpg" />
+		<img id="homePics" src="./pictures/4.jpg" />
+		<img id="homePics" src="./pictures/5.jpg" />
+		<img id="homePics" src="./pictures/6.jpg" />
+		<img id="homePics" src="./pictures/7.jpg" />
+		<img id="homePics" src="./pictures/8.jpg" />
+		<img id="homePics" src="./pictures/9.jpg" />
+		<img id="homePics" src="./pictures/10.jpg" />
 </div>
 	</body>
 </html>
